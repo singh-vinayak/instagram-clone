be3d600715f268082b98918708f4acf67db50a37
Vinayak Singh <vinayaksingh5302@gmail.com>
2021-09-15 01:34:49 +0530

    use user hook

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import useUser from '../../hooks/use-user';
 
 export default function Sidebar() {
+    const { user: { docId, userId, following, username, fullName } = {} } = useUser();
+    //console.log(following)
     return (<p>I am the sidebar</p>);
 }
