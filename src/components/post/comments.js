c2c993e1d4e18ce63f3881ec8e145133bfc3b6a6
Vinayak Singh <vinayaksingh5302@gmail.com>
2021-09-18 23:51:44 +0530

    added onclick functionality

--- src/components/post/comments.js
+++ src/components/post/comments.js
@@ -5,16 +5,17 @@ import AddComment from './add-comment';
 
 export default function Comments({ docId, comments: allComments, posted, commentInput }) {
     const [comments, setComments] = useState(allComments);
+    const [commentsToShow, setCommentsToShow]= useState(3)
 
     return (
         <>
             <div className="p-4 pt-1 pb-4">
                 {comments.length >= 3 && (
-                    <p className="text-sm text-gray-500 mb-1 cursor-pointer">
+                    <p className="text-sm text-gray-500 mb-1 cursor-pointer" onClick={()=> setCommentsToShow(comments.length)}>
                         View all {comments.length} comments
                     </p>
                 )}
-                {comments.slice(0, 3).map((item) => (
+                {comments.slice(0, commentsToShow).map((item) => (
                     <p key={`${item.comment}-${item.displayName}`} className="mb-1">
                         <Link to={`/p/${item.displayName}`}>
                             <span className="mr-1 font-bold">{item.displayName}</span>
